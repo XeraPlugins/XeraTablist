4ef5d9e14ad73e1f754b8e4e3b91ac8785d21546
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-10-15 16:09:53 +0200

    Change lots of stuff in the code

---
+++ src/main/java/Xera/Tablist/Tablist.java
@@ -0,0 +1,46 @@
+package Xera.Tablist;
+
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class Tablist implements Runnable {
+    XeraTablist xeraTablist;
+
+    public Tablist(XeraTablist xeraTablist) {
+        this.xeraTablist = xeraTablist;
+    }
+
+    public void run() {
+        if (Bukkit.getOnlinePlayers().size() == 0) {
+            return;
+        }
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            StringBuilder head = new StringBuilder();
+            StringBuilder footer = new StringBuilder();
+            List<String> headerlist = xeraTablist.getConfig().getStringList("tablist.header");
+            List<String> footerlist = xeraTablist.getConfig().getStringList("tablist.footer");
+
+            for (int i = 0; i < headerlist.size(); i++) {
+                if (i == (headerlist.size() - 1)) {
+                    head.append(headerlist.get(i));
+                } else {
+                    head.append(headerlist.get(i)).append("\n");
+                }
+            }
+
+            for (int i = 0; i < footerlist.size(); i++) {
+                if (i == (footerlist.size() - 1)) {
+                    footer.append(footerlist.get(i));
+                } else {
+                    footer.append(footerlist.get(i)).append("\n");
+                }
+            }
+
+            player.setPlayerListHeaderFooter(new ComponentBuilder(XeraTablist.parseText(player, head.toString())).create(), new ComponentBuilder(XeraTablist.parseText(player, footer.toString())).create());
+        }
+    }
+}
--- src/main/java/Xera/Tablist/XeraTablist.java
+++ src/main/java/Xera/Tablist/XeraTablist.java
@@ -1,62 +1,38 @@
 package Xera.Tablist;
 
-import Xera.Tablist.Tablist.TabUtil;
-import Xera.Tablist.Tablist.Tablist;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Logger;
 
 public class XeraTablist extends JavaPlugin implements Listener {
-    FileConfiguration config = getConfig();
     public static long starttime;
-    public static boolean haspapi = false;
 
     public void onEnable() {
         Logger log = getLogger();
 
         log.info("Loading config");
-        config.getString("tablist.header");
-        config.getString("tablist.footer");
-        config.options().copyDefaults(true);
-        saveConfig();
+        saveDefaultConfig();
 
         starttime = System.currentTimeMillis();
         this.getCommand("tabrconfig").setExecutor(new ReloadCommand(this));
-        Bukkit.getScheduler().runTaskTimer(this, new Tablist(this), 0, 10L);
-
-        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            haspapi = true;
-        }
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Tablist(this), 0, 20);
 
         log.info("XeraTablist enabled");
     }
 
-    public static String parseText(Player player, String text) throws NoSuchFieldException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        String newtext = text;
-        Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
-        int ping = (Integer) entityPlayer.getClass().getField("ping").get(entityPlayer);
+    public static String parseText(Player player, String text) {
+        String newtext;
 
         // PAPI
-        if (haspapi) {
-            newtext = PlaceholderAPI.setPlaceholders(player, text);
-        }
+        newtext = PlaceholderAPI.setPlaceholders(player, text);
 
         // ChatColor
         newtext = ChatColor.translateAlternateColorCodes('&', newtext);
-
-        // Custom Placeholders
-        newtext = newtext
-                .replaceAll("%tps%", TabUtil.getTps())
-                .replaceAll("%ping%", String.valueOf(ping))
-                .replaceAll("%uptime%", TabUtil.GetFormattedInterval(System.currentTimeMillis() - XeraTablist.starttime))
-                .replaceAll("%players%", Integer.toString(Bukkit.getServer().getOnlinePlayers().size()));
 
         return newtext;
     }
